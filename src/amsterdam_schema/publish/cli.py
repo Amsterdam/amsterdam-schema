00b8645a478556985913d8d2945112cc2096c5f1
Guido Kollerie <guido@kollerie.com>
2021-04-06 06:51:50 +0200

    AB#5020 Turn everything into a proper Python package.
    In order to be able to publish specific revisions of the schemas in this
    repo we could have mucked around with a Docker build that captures the
    state of the repo into a specific build. Or we could choose to capture
    the state of the repo into a versioned Python package. We chose the
    latter. This involved the following steps:
    
    - specifying setup.cfg and pyproject.toml files
    - added tbump
    - ditching requirements.* (they are now in setup.cfg)
    - moving publish.py into a Python package with same name as this repo
    - bundling schemas with the resulting Python package
    
    The latter proved to be rather challenging as the schemas live outside of
    the Python package (eg they are not in a directory with a __init__.py
    file). Moving them all into the newly created `amsterdam_schema` package
    (under the `src` directory) would probably have confused other users of
    this repo that expect the schemas to be in the root of the repo. Hence
    that was not an option.
    
    Initially it seemed that with a proper `MANIFEST.in` file and
    `include_package_data = True` in `setup.cfg` the schemas (and datasets)
    could be included. The `sdist` had everything we had specified in it.
    But the `wheel` did not. Not building a wheel these days is not really
    an option. Hence we had to look for a different option.
    
    In the old days we could have used the `data_files` directive, though
    that is currently being frowned upon in modern Python packaging. Still
    we considered that option. The problem with `data_files` is that each
    file needs to be specified separately as wildcards are not supported.
    That is a problem in `setup.cfg` only distributions, as we can't use
    Python code to dynamically build up the `data_files` data structure. So
    we ditched that idea as well.
    
    In hindsight the solution was simple. Git supports symbolic links and
    Python packaging properly processes them. Hence we included symlinks to
    the top level `datasets` and `schemas@*` directories from within the
    `amsterdam_schema` package. Now we have the best of both worlds:
    
    - data modelling users still find the schemas in a familiar location
     (root dir of the repo)
    - and us, developers, that are responsible of publishing these schema's
      using Python scripts, can properly package these datasets and schemas
      together with the publishing code.
    
    NOTE: the `MANIFEST.in` is still needed. Although `sdist`s and `wheel`s
    build properly without it, a simple `pip install .` fails to include the
    schemas without it.
    
    The Docker build is now rather simple. Instead of having knowledge where
    the `publish` script expects the datasets and schemas, we can simply
    build the Python package and install it. The Python package (the publish
    in it) knows where everything is regardless of what the Docker build
    did.
    
    While we are at making this a proper Python package we have turned up
    the linting and static type checking a couple of notches. This to bring
    it more in line with what we are doing for the `schema-tools` package
    (and hopefully others)
    
    The `--use-local` option to the `publish` command now works quite
    differently. Instead of working directly with the file system from a
    cloned repo, it works with package resources. It the primary reason for
    this whole packaging excercise.
    
    NOTE regarding not havening `twine` included: We don't expect this
    package to be published to PyPi. The package is only used in internal
    builds.
    
    The name mangling is weird. We should ditch it!

--- src/amsterdam_schema/publish/cli.py
+++ src/amsterdam_schema/publish/cli.py
@@ -4,25 +4,27 @@ Script that publishes verified schemas to the objectstore.
 
 Course of action is as follows:
 
-* A zipfile of the master branch of the 'amsterdam-schema' repo is fetched from github
-* The zip is unpacked to a temporary directory
 * The relevant schema files (metaschema + datasets) are pushed to the objectstore
 * An json file with an index of the datasets is pushed to the the objectstore
-* It is possible to publish the local schema files to the objectstore using the --use-local flag
 
 """
+
+import functools
 import json
 import logging
-from io import BytesIO
 import os
-from pathlib import Path
+import shutil
+from importlib import resources
+from io import BytesIO
 from os.path import splitext
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Callable, ContextManager, Iterator, List
+from zipfile import ZipFile
+
 import click
 import in_place
 import requests
-from tempfile import TemporaryDirectory
-import shutil
-from zipfile import ZipFile
 from swiftclient.service import SwiftError, SwiftService, SwiftUploadObject
 
 logger = logging.getLogger("__name__")
@@ -32,7 +34,7 @@ publishable_prefixes = ("datasets", "schema@")
 DEFAULT_BASE_URL = "https://schemas.data.amsterdam.nl"
 
 
-def fetch_publishable_paths(paths):
+def fetch_publishable_paths(paths: List[str]) -> List[List[str]]:
     publishable_paths = []
     for path in paths:
         path_parts = path.split("/")
@@ -41,9 +43,9 @@ def fetch_publishable_paths(paths):
     return publishable_paths
 
 
-def extract_and_fetch_paths(github_url, temp_dir):
+def extract_and_fetch_paths(github_url: str, temp_dir: Path) -> List[List[str]]:
     response = requests.get(github_url, stream=True)
-    tmp_file = Path(temp_dir) / "out.zip"
+    tmp_file = temp_dir / "out.zip"
     with open(tmp_file, "wb") as wf:
         shutil.copyfileobj(response.raw, wf)
 
@@ -56,33 +58,54 @@ def extract_and_fetch_paths(github_url, temp_dir):
     return publishable_paths
 
 
-def fetch_repo_root():
-    return Path(__file__).resolve().parents[1]
+def fetch_local_as_publishable(root_pkg_name: str, temp_dir_path: Path) -> List[List[str]]:
+    """Fetch publishable artifacts from package as path components.
 
+    Not sure how to describe this function best. I've kept the API, or at least that what it
+    returns, identical to its predecessor. It copies the publishable artifacts from the package
+    to the ``temp_dir_path`` and returns all relative paths from ``temp_dir_path`` down as a
+    list of strings where each string represents a path component.
 
-def fetch_local_as_publishable(repo_root, temp_dir):
-    repo_root_name = repo_root.name
+    Args:
+        root_pkg_name: Name of the root package (eg "amsterdam_schema"
+        temp_dir_path: Path of the temporary directory to copy publishable artifacts to
 
-    publishable_paths = []
-    temp_dir_path = Path(temp_dir)
-    temp_repo_path = temp_dir_path / repo_root_name
+    Returns:
+        A list of publishable artifacts in the ``temp_dir_path``. Each publishable
+        artifacts is identified by a list of its path components from `temp_dir_path`` down
+        (eg it does not include the path component of ``temp_dir_path``)
 
-    # Copy entire repo to temp_dir, because we want to modify schema_base_url in place
-    shutil.copytree(repo_root, temp_repo_path, dirs_exist_ok=True)
+    """
+    dst_path = temp_dir_path / root_pkg_name
 
-    for subdir in temp_repo_path.iterdir():
-        if subdir.is_dir() and subdir.name.startswith(publishable_prefixes):
-            for dirpath, dirnames, filenames in os.walk(subdir):
-                if filenames:
-                    subpath = list(Path(dirpath).relative_to(temp_repo_path).parts)
-                    publishable_paths.extend(
-                        [[repo_root_name] + subpath + [fn] for fn in filenames]
-                    )
+    # Use importlib to extract resources from the package. This works for regularly installed
+    # packages as well as editable installed packages.
+    resource_names: Iterator[str] = filter(
+        lambda res: any(map(lambda pre: res.startswith(pre), publishable_prefixes)),
+        resources.contents(root_pkg_name),
+    )
+    resource_path_func: Callable[[str], ContextManager[Path]] = functools.partial(
+        resources.path, root_pkg_name
+    )
+    res_path_ctx_mgrs: Iterator[ContextManager[Path]] = map(resource_path_func, resource_names)
+
+    for rpcm in res_path_ctx_mgrs:
+        with rpcm as dir_path:
+            # if the package is a zip file the context manager unzips the resource (`dir_path`)
+            # into a temporary directory.
+            shutil.copytree(
+                dir_path.as_posix(), (dst_path / dir_path.name).as_posix(), dirs_exist_ok=True
+            )
+
+    publishable_paths: List[List[str]] = []
+    for schema_path in dst_path.glob("**/*json"):
+        sub_path = schema_path.relative_to(dst_path.parent)
+        publishable_paths.append(list(sub_path.parts))
 
     return publishable_paths
 
 
-def get_index_file_obj(publishable_paths):
+def get_index_file_obj(publishable_paths: List[List[str]]) -> BytesIO:
     index = {}
     for path_parts in publishable_paths:
         if path_parts[1] != "datasets":
@@ -94,8 +117,10 @@ def get_index_file_obj(publishable_paths):
     return BytesIO(json.dumps(index).encode("utf-8"))
 
 
-def create_object_name(path_parts):
-    """We need some path mangling and move objects from version directories
+def create_object_name(path_parts: List[str]) -> str:
+    """Mangle path names.
+
+    We need some path mangling and move objects from version directories
     to the root with an @vx.y.z postfix, e.g.:
         schema@v1.1.1/row-meta-schema -> row-meta-schema@v1.1.1
         schema@v1.1.1/meta/auth -> meta/auth@v1.1.1
@@ -111,12 +136,12 @@ def create_object_name(path_parts):
     return "/".join(parts[1:])
 
 
-def replace_schema_base_url(temp_dir, schema_base_url):
-    """Do a replacement of the schema_base_url, needed to
-    valdidate schemas when served from another base url,
-    e.g. for testing
+def replace_schema_base_url(temp_dir: Path, schema_base_url: str) -> None:
+    """Do a replacement of the schema_base_url.
+
+    Needed to valdidate schemas when served from another base url, e.g. for testing.
     """
-    for root, dirs, file_names in os.walk(temp_dir):
+    for root, _dirs, file_names in os.walk(temp_dir):
         root_path = Path(root)
         for file_name in file_names:
             file_path = root_path / file_name
@@ -150,27 +175,23 @@ def replace_schema_base_url(temp_dir, schema_base_url):
     is_flag=True,
     help="Use local schema files, not the github zip archive",
 )
-def main(dp_env, github_url, schema_base_url, use_local):
-
+def main(dp_env: str, github_url: str, schema_base_url: str, use_local: bool) -> None:
     # We extract the zip, because otherwise we need a big set
     # of open file handles during upload, now we can use file-paths
     with TemporaryDirectory() as temp_dir:
         files_root = Path(temp_dir)
         if use_local:
-            repo_root = fetch_repo_root()
-            publishable_paths = fetch_local_as_publishable(repo_root, temp_dir)
+            publishable_paths = fetch_local_as_publishable("amsterdam_schema", files_root)
         else:
-            publishable_paths = extract_and_fetch_paths(github_url, temp_dir)
+            publishable_paths = extract_and_fetch_paths(github_url, files_root)
 
         if schema_base_url is not None:
-            replace_schema_base_url(temp_dir, schema_base_url)
+            replace_schema_base_url(files_root, schema_base_url)
         index_file_obj = get_index_file_obj(publishable_paths)
 
         with SwiftService() as swift:
             try:
-                uploads = [
-                    SwiftUploadObject(index_file_obj, object_name="datasets/index.json")
-                ]
+                uploads = [SwiftUploadObject(index_file_obj, object_name="datasets/index.json")]
                 for path_parts in publishable_paths:
                     object_name = create_object_name(path_parts)
                     uploads.append(
