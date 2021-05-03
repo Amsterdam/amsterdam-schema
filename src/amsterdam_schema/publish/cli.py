899958eb042b10e0af43cd005ff8b7a6cd253044
Chris van Riel <c.van.riel@amsterdam.nl>
2021-05-03 13:46:53 +0200

    revert path schema logic

--- src/amsterdam_schema/publish/cli.py
+++ src/amsterdam_schema/publish/cli.py
@@ -87,7 +87,7 @@ def get_index_file_obj(publishable_paths: List[List[str]]) -> BytesIO:
         dataset_ext = path_parts[-1]
         folder = "/".join(path_parts[2:-1])
         dataset = splitext(dataset_ext)[0]
-        index[folder] = f"{folder}/{dataset}"
+        index[dataset] = f"{folder}/{dataset}"
     return BytesIO(json.dumps(index).encode("utf-8"))
 
 
