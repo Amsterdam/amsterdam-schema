0b2bfad69b9c5fa24b494183b8bbe810da812fb8
arbaco <l.arbaczewski@amsterdam.nl>
2023-08-29 14:25:48 +0200

    Relation with Verblijfsobject ID
    Relation with Verblijfsobject ID for BRK kadastraleobjecten

--- datasets/dataverkenner/kadastraleobjecten/dataset.sql
+++ datasets/dataverkenner/kadastraleobjecten/dataset.sql
@@ -1,11 +1,12 @@
 create or replace view public.dataverkenner_kadastraleobjecten_kadastraleobjecten WITH (security_barrier) as
 select 
-brk_kadastraleobjecten.id as "id",
-brk_kadastraleobjecten.identificatie as "identificatie",
-brk_kadastraleobjecten.neuron_id as "neuron_id",
-brk_kadastraleobjecten.volgnummer as "volgnummer",
-brk_kadastraleobjecten.kadastrale_aanduiding as "kadastrale_aanduiding",
-brk_kadastraleobjecten.begin_geldigheid as "begin_geldigheid",
-brk_kadastraleobjecten.eind_geldigheid as "eind_geldigheid"
-from brk_kadastraleobjecten
-left join brk_kadastraleobjecten_hft_rel_mt_vot on brk_kadastraleobjecten.id = kadastraleobjecten_id
+brk_2_kadastraleobjecten.id as "id",
+brk_2_kadastraleobjecten.identificatie as "identificatie",
+brk_2_kadastraleobjecten.neuron_id as "neuron_id",
+brk_2_kadastraleobjecten.volgnummer as "volgnummer",
+brk_2_kadastraleobjecten.kadastrale_aanduiding as "kadastrale_aanduiding",
+brk_2_kadastraleobjecten.begin_geldigheid as "begin_geldigheid",
+brk_2_kadastraleobjecten.eind_geldigheid as "eind_geldigheid",
+brk_2_kadastraleobjecten_hft_rel_mt_vot.hft_rel_mt_vot_identificatie as "heeft_een_relatie_met_bag_verblijfsobject_identificatie"
+from brk_2_kadastraleobjecten
+left join brk_2_kadastraleobjecten_hft_rel_mt_vot on brk_2_kadastraleobjecten.id = kadastraleobjecten_id
