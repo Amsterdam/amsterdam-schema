5c9c3acfe7aac425018d0f3b36646361fcff5a48
arbaco <l.arbaczewski@amsterdam.nl>
2023-08-31 16:17:08 +0200

    Betrokken bij Views BRK 2
    Betrokken bij Views BRK 2

---
+++ datasets/dataverkenner/betrokkenbij/dataset.sql
@@ -0,0 +1,16 @@
+create or replace view public.dataverkenner_betrokkenbij_betrokkenbij WITH (security_barrier) as
+select 
+brk_2_kadastraleobjecten.id as "id",
+brk_2_kadastraleobjecten.identificatie as "identificatie",
+brk_2_kadastraleobjecten.neuron_id as "neuron_id",
+brk_2_kadastraleobjecten.volgnummer as "volgnummer",
+brk_2_kadastraleobjecten.kadastrale_aanduiding as "kadastrale_aanduiding",
+brk_2_kadastraleobjecten.begin_geldigheid as "begin_geldigheid",
+brk_2_kadastraleobjecten.eind_geldigheid as "eind_geldigheid",
+brk_2_kadastraleobjecten_is_ontstaan_uit_brk_g_perceel.kadastraleobjecten_id as "is_ontstaan_uit_brk_g_perceel_id",
+brk_2_kadastraleobjecten_is_ontstaan_uit_brk_g_perceel.kadastraleobjecten_identificatie as "is_ontstaan_uit_brk_g_perceel_identificatie",
+brk_2_kadastraleobjecten_is_ontstaan_uit_brk_g_perceel.kadastraleobjecten_volgnummer as "is_ontstaan_uit_brk_g_perceel_volgnummer",
+brk_2_kadastraleobjecten_is_ontstaan_uit_brk_g_perceel.begin_geldigheid as "is_ontstaan_uit_brk_g_perceel_begin_geldigheid",
+brk_2_kadastraleobjecten_is_ontstaan_uit_brk_g_perceel.eind_geldigheid as "is_ontstaan_uit_brk_g_perceel_eind_geldigheid"
+from brk_2_kadastraleobjecten
+left join brk_2_kadastraleobjecten_is_ontstaan_uit_brk_g_perceel on brk_2_kadastraleobjecten.id = kadastraleobjecten_id
