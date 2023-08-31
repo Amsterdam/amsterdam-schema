30aaa0ce44edabb789b6c1c5e542db53624d5f7a
arbaco <l.arbaczewski@amsterdam.nl>
2023-08-31 13:37:30 +0200

    Data verkenner Views BRK2 tenaamstellingen and zakleijkerechten update
    Data verkenner Views BRK2 tenaamstellingen and zakleijkerechten update

--- datasets/dataverkenner/zakelijkerechten/dataset.sql
+++ datasets/dataverkenner/zakelijkerechten/dataset.sql
@@ -5,6 +5,7 @@ brk_2_zakelijkerechten.identificatie as "identificatie",
 brk_2_zakelijkerechten.volgnummer as "volgnummer",
 brk_2_zakelijkerechten.rust_op_brk_kadastraal_object_identificatie as "rust_op_brk_kadastraal_object_identificatie",
 brk_2_zakelijkerechten.aard_zakelijk_recht_omschrijving as "aard_zakelijk_recht_omschrijving",
+brk_2_zakelijkerechten.betrokken_bij_appartementsrechtsplitsing_vve as "betrokken_bij_appartementsrechtsplitsing_vve",
 brk_2_zakelijkerechten.begin_geldigheid as "begin_geldigheid",
 brk_2_zakelijkerechten.eind_geldigheid as "eind_geldigheid"
 from brk_2_zakelijkerechten
