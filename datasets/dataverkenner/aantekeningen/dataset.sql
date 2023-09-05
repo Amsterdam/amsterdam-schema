13614169744642eed0c5abf5ba85b05994c5b549
arbaco <l.arbaczewski@amsterdam.nl>
2023-09-05 16:43:23 +0200

    Update aantekeningen views BRK2
    Update aantekeningen views BRK2

--- datasets/dataverkenner/aantekeningen/dataset.sql
+++ datasets/dataverkenner/aantekeningen/dataset.sql
@@ -4,6 +4,7 @@ brk_2_aantekeningenkadastraleobjecten.id as "id",
 brk_2_aantekeningenkadastraleobjecten.identificatie as "identificatie",
 brk_2_aantekeningenkadastraleobjecten.volgnummer as "volgnummer",
 brk_2_aantekeningenkadastraleobjecten.aard_omschrijving as "aard_omschrijving",
+brk_2_aantekeningenkadastraleobjecten.hft_btrk_op_kot_identificatie = as "hft_btrk_op_kot_identificatie",
 brk_2_aantekeningenkadastraleobjecten.begin_geldigheid as "begin_geldigheid",
 brk_2_aantekeningenkadastraleobjecten.eind_geldigheid as "eind_geldigheid"
 from brk_2_aantekeningenkadastraleobjecten
