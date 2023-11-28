6ae60c51998db53dca20da15553218493fd8419e
arbacom <l.arbaczewski@amsterdam.nl>
2023-11-28 12:00:39 +0100

    fix params in views

--- datasets/dataverkenner/kadastraleobject/dataset.sql
+++ datasets/dataverkenner/kadastraleobject/dataset.sql
@@ -9,9 +9,8 @@ z.soort_cultuur_onbebouwd_code as "soort_cultuur_onbebouwd_code",
 z.soort_cultuur_onbebouwd_omschrijving as "soort_cultuur_onbebouwd_omschrijving",
 z.soort_cultuur_bebouwd_code as "soort_cultuur_bebouwd_code",
 z.soort_cultuur_bebouwd_omschrijving as "soort_cultuur_bebouwd_omschrijving",
-a.begin_geldigheid as "ont_uit_begin_geldigheid",
-a.eind_geldigheid as "ont_uit_eind_geldigheid",
+z.begin_geldigheid as "begin_geldigheid",
+z.eind_geldigheid as "eind_geldigheid",
 b.hft_rel_mt_vot_identificatie as "heeft_een_relatie_met_bag_verblijfsobject_identificatie"
 from brk_2_kadastraleobjecten z
-left join brk_2_kadastraleobjecten_is_ontstaan_uit_brk_kadastraalobject a on z.id = a.kadastraleobjecten_id
 left join brk_2_kadastraleobjecten_hft_rel_mt_vot b on z.id = b.kadastraleobjecten_id;
