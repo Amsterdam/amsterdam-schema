ed9fe00020e37e98ef2cff989243097a30038d69
arbacom <l.arbaczewski@amsterdam.nl>
2023-11-30 10:22:47 +0100

    Expand zakelijkerechten view relation to kadastralesubjecten

--- datasets/dataverkenner/zakelijkerechten/dataset.sql
+++ datasets/dataverkenner/zakelijkerechten/dataset.sql
@@ -6,6 +6,8 @@ SELECT
     z.rust_op_brk_kadastraal_object_identificatie AS "rust_op_brk_kadastraal_object_identificatie",
     z.aard_zakelijk_recht_omschrijving AS "aard_zakelijk_recht_omschrijving",
     z.betrokken_bij_appartementsrechtsplitsing_vve AS "betrokken_bij_appartementsrechtsplitsing_vve",
+    z.vve_identificatie_ontstaan_uit_id AS "vve_identificatie_ontstaan_uit_id",
+    z.vve_identificatie_betrokken_bij_id AS "vve_identificatie_betrokken_bij_id",
     z.begin_geldigheid AS "begin_geldigheid",
     z.eind_geldigheid AS "eind_geldigheid"
 FROM 
