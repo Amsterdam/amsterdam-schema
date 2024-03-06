d01c60f8e6742b284b98fb932e0d0d61506f7715
Jeroenriemens <j.riemens@amsterdam.nl>
2024-03-06 15:57:46 +0100

    changed m and views for betr_bij and ontst_uit

--- datasets/dataverkenner/isontstaanuit/dataset.sql
+++ datasets/dataverkenner/isontstaanuit/dataset.sql
@@ -1,16 +1,19 @@
 create or replace view public.dataverkenner_isontstaanuit_isontstaanuit WITH (security_barrier) as
-select kot.id as "id",
-kot.identificatie as "identificatie",
-kot.neuron_id as "neuron_id",
-kot.volgnummer as "volgnummer",
-kot.kadastrale_aanduiding as "kadastrale_aanduiding",
+SELECT kot.id AS "id",
+kot.identificatie AS "identificatie",
+kot.neuron_id AS "neuron_id",
+kot.volgnummer AS "volgnummer",
+kot.kadastrale_aanduiding AS "kadastrale_aanduiding",
 ontstaan_uit.is_ontstaan_uit_brk_kadastraalobject_identificatie AS "is_ontstaan_uit_kadastraalobjecten_identificatie",
 ontstaan_uit.is_ontstaan_uit_brk_kadastraalobject_id AS "is_ontstaan_uit_kadastraalobjecten_id",
-ontstaan_uit.is_ontstaan_uit_brk_kadastraalobject_volgnummer AS "is_ontstaan_uit_kadastraalobjecten_volgnummer"
-from brk_2_kadastraleobjecten kot
+ontstaan_uit.is_ontstaan_uit_brk_kadastraalobject_volgnummer AS "is_ontstaan_uit_kadastraalobjecten_volgnummer",
+kot2.kadastrale_aanduiding AS "is_ontstaan_uit_kadastrale_aanduiding"
+FROM brk_2_kadastraleobjecten kot
 LEFT JOIN 
-(SELECT *
-FROM brk_2_kadastraleobjecten_is_ontstaan_uit_brk_kadastraalobject
-WHERE eind_geldigheid IS NULL) ontstaan_uit
+  (SELECT *
+   FROM brk_2_kadastraleobjecten_is_ontstaan_uit_brk_kadastraalobject
+   WHERE eind_geldigheid IS NULL) ontstaan_uit
+LEFT JOIN brk_2_kadastraleobjecten kot2 ON ontstaan_uit.is_ontstaan_uit_brk_kadastraalobject_id=kot2.id 
 ON  kot.id=ontstaan_uit.kadastraleobjecten_id
-WHERE kot.datum_actueel_tot IS NULL;
+WHERE kot.datum_actueel_tot IS NULL
+AND kot2.datum_actueel_tot IS NULL;
