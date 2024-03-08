6dff9d00232b1f36517899ce610527de50ecf4be
Jeroen Riemens <j.riemens@amsterdam.nl>
2024-03-08 10:59:26 +0100

    changed m and views for betr_bij and ontst_uit (#976)

--- datasets/dataverkenner/betrokkenbijm/dataset.sql
+++ datasets/dataverkenner/betrokkenbijm/dataset.sql
@@ -1,18 +1,17 @@
 CREATE MATERIALIZED VIEW IF NOT EXISTS public.dataverkenner_betrokkenbijm_betrokkenbij as
-select kot.id as "id",
-kot.identificatie as "identificatie",
-kot.neuron_id as "neuron_id",
-kot.volgnummer as "volgnummer",
-kot.kadastrale_aanduiding as "kadastrale_aanduiding",
-betrokken_bij.kadastraleobjecten_id as "betrokken_bij_kadastraleobjecten_id",
-betrokken_bij.kadastraleobjecten_identificatie as "betrokken_bij_identificatie",
-betrokken_bij.kadastraleobjecten_volgnummer as "betrokken_bij_volgnummer"
-from brk_2_kadastraleobjecten kot
-LEFT JOIN 
-(SELECT *
-FROM brk_2_kadastraleobjecten_is_ontstaan_uit_brk_kadastraalobject
-WHERE eind_geldigheid IS NULL) betrokken_bij
-ON kot.id=betrokken_bij.is_ontstaan_uit_brk_kadastraalobject_id
-WHERE kot.datum_actueel_tot IS NULL
-with no data
+SELECT 
+    kot1.identificatie,
+    kot1.volgnummer,
+    kot1.kadastrale_aanduiding,
+    kot1.neuron_id,
+    ontstaan_uit.kadastraleobjecten_id AS betrokken_bij_kadastraleobjecten_id,
+    ontstaan_uit.kadastraleobjecten_identificatie AS betrokken_bij_identificatie,
+    ontstaan_uit.kadastraleobjecten_volgnummer AS betrokken_bij_volgnummer,
+    kot2.kadastrale_aanduiding AS betrokken_bij_kadastrale_aanduiding
+FROM brk_2_kadastraleobjecten_is_ontstaan_uit_brk_kadastraalobject ontstaan_uit
+LEFT JOIN brk_2_kadastraleobjecten kot1 ON ontstaan_uit.is_ontstaan_uit_brk_kadastraalobject_id ::TEXT=kot1.id::TEXT
+LEFT JOIN brk_2_kadastraleobjecten  kot2  ON ontstaan_uit.kadastraleobjecten_id ::TEXT=kot2.id ::TEXT
+WHERE ontstaan_uit.eind_geldigheid IS NULL
+AND kot1.datum_actueel_tot IS NULL
+AND kot2.datum_actueel_tot IS NULL
 ;
