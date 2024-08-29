2465743669f9c9d425a5d53e512c38a86e08b42e
Lukas Arbaczewski <129159529+arbaco@users.noreply.github.com>
2024-08-29 12:15:17 +0200

    Lukas/expand panden (#1118)
    * Relatie naar verblijfsobject(en)
    
    Relatie naar verblijfsobject(en)
    
    * Update dataset.sql
    
    Update dataset.sql
    
    * groups query update for verblijfsobjecten_identificaties in panden mv
    
    groups query update for verblijfsobjecten_identificaties in panden mv
    
    * Fixed query

--- datasets/dataverkenner/bagpandenm/dataset.sql
+++ datasets/dataverkenner/bagpandenm/dataset.sql
@@ -1,22 +1,41 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS public.dataverkenner_bagpandenm_bagpanden as
-select
-bag_panden.id as "id",
-bag_panden.identificatie as "pand_identificatie",
-bag_panden.volgnummer as "pand_volgnummer",
-bag_panden.geometrie as "pand_geometrie",
-bag_panden.oorspronkelijk_bouwjaar as "pand_oorspronkelijk_bouwjaar",
-bag_panden.naam as "pand_naam",
-bag_panden.geconstateerd as "pand_geconstateerd",
-bag_panden.status_omschrijving as "pand_status_omschrijving",
-bag_panden.ligging_omschrijving as "pand_ligging_omschrijving",
-bag_panden.type_woonobject as "pand_type_woonobject",
-bag_panden.aantal_bouwlagen as "pand_aantal_bouwlagen",
-bag_panden.hoogste_bouwlaag as "pand_hoogste_bouwlaag",
-bag_panden.laagste_bouwlaag as "pand_laagste_bouwlaag",
-bag_panden.documentdatum as "pand_documentdatum",
-bag_panden.documentnummer as "pand_documentnummer",
-bag_panden.begin_geldigheid as "pand_begin_geldigheid",
-bag_panden.eind_geldigheid as "pand_eind_geldigheid"
-from bag_panden
-where bag_panden.eind_geldigheid is null 
-and bag_panden.status_omschrijving in ('Bouwaanvraag ontvangen','Bouwvergunning verleend','Bouw gestart','Pand in gebruik (niet ingemeten)','Pand in gebruik','Sloopvergunning verleend','Pand buiten gebruik','Verbouwing pand') with no data;
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.dataverkenner_bagpandenm_bagpanden AS WITH verblijfsobjecten AS
+  ( SELECT ligt_in_panden_id,
+           array_agg(verblijfsobjecten_identificatie) AS vots
+   FROM bag_verblijfsobjecten_ligt_in_panden
+   LEFT JOIN bag_verblijfsobjecten vot ON bag_verblijfsobjecten_ligt_in_panden.verblijfsobjecten_id = vot.id
+   WHERE bag_verblijfsobjecten_ligt_in_panden.eind_geldigheid IS NULL
+     AND vot.status_omschrijving IN ('Verblijfsobject gevormd',
+                                     'Verblijfsobject in gebruik (niet ingemeten)',
+                                     'Verblijfsobject in gebruik',
+                                     'Verblijfsobject buiten gebruik',
+                                     'Verbouwing Verblijfsobject')
+   GROUP BY ligt_in_panden_id)
+SELECT bag_panden.id AS "id",
+       bag_panden.identificatie AS "pand_identificatie",
+       bag_panden.volgnummer AS "pand_volgnummer",
+       bag_panden.geometrie AS "pand_geometrie",
+       bag_panden.oorspronkelijk_bouwjaar AS "pand_oorspronkelijk_bouwjaar",
+       bag_panden.naam AS "pand_naam",
+       bag_panden.geconstateerd AS "pand_geconstateerd",
+       bag_panden.status_omschrijving AS "pand_status_omschrijving",
+       bag_panden.ligging_omschrijving AS "pand_ligging_omschrijving",
+       bag_panden.type_woonobject AS "pand_type_woonobject",
+       bag_panden.aantal_bouwlagen AS "pand_aantal_bouwlagen",
+       bag_panden.hoogste_bouwlaag AS "pand_hoogste_bouwlaag",
+       bag_panden.laagste_bouwlaag AS "pand_laagste_bouwlaag",
+       bag_panden.documentdatum AS "pand_documentdatum",
+       bag_panden.documentnummer AS "pand_documentnummer",
+       bag_panden.begin_geldigheid AS "pand_begin_geldigheid",
+       bag_panden.eind_geldigheid AS "pand_eind_geldigheid",
+       verblijfsobjecten.vots::_VARCHAR AS "verblijfsobjecten_identificaties"
+FROM bag_panden
+LEFT JOIN verblijfsobjecten ON bag_panden.id = verblijfsobjecten.ligt_in_panden_id
+WHERE bag_panden.eind_geldigheid IS NULL
+  AND bag_panden.status_omschrijving IN ('Bouwaanvraag ontvangen',
+                                         'Bouwvergunning verleend',
+                                         'Bouw gestart',
+                                         'Pand in gebruik (niet ingemeten)',
+                                         'Pand in gebruik',
+                                         'Sloopvergunning verleend',
+                                         'Pand buiten gebruik',
+                                         'Verbouwing pand') WITH NO DATA;
