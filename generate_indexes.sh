886734633d000d4794779f0e8e875bb18b9a5897
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-02-17 13:43:29 +0100

    Add scopes list json (#1271)
    * add command to create list of scopes
    
    * autogenerate file
    
    * add scopes.json
    
    * update docstring
    
    * DRY up code with _write_json

--- generate_indexes.sh
+++ generate_indexes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 generate-scope-index > scopes/index.json
+generate-scope-list > scopes/scopes.json
 generate-access-package-list > scopes/packages.json
 generate-publisher-index > publishers/index.json
