f85bb075260f94fcf255468a8f561a25079ef8ad
yrob <y.robert@amsterdam.nl>
2026-01-06 13:21:57 +0100

    Show relations for all versions in relation graph

--- scripts/relations_graph.py
+++ scripts/relations_graph.py
@@ -26,26 +26,26 @@ for dirpath, _, filenames in os.walk("datasets/"):
             continue
 
         print(d["id"])
+        for version in d["versions"]:
+            for table in d["versions"][version]["tables"]:
+                if "$ref" in table:
+                    with open(os.path.join(dirpath, table["$ref"] + ".json")) as f:
+                        table = json.load(f)
 
-        for table in d["versions"][d["defaultVersion"]]["tables"]:
-            if "$ref" in table:
-                with open(os.path.join(dirpath, table["$ref"] + ".json")) as f:
-                    table = json.load(f)
+                for _, prop in table["schema"]["properties"].items():
+                    rel = prop.get("relation")
+                    if not rel:
+                        continue
 
-            for _, prop in table["schema"]["properties"].items():
-                rel = prop.get("relation")
-                if not rel:
-                    continue
+                    to_ds, to_table = rel.split(":")
+                    rel = f"{d['id']} -> {to_ds}"
+                    if False:  # TODO Prettify output and enable.
+                        rel += f' [label="{table["id"]} -> {to_table}"];'
 
-                to_ds, to_table = rel.split(":")
-                rel = f"{d['id']} -> {to_ds}"
-                if False:  # TODO Prettify output and enable.
-                    rel += f' [label="{table["id"]} -> {to_table}"];'
+                    if rel in seen:
+                        continue
+                    seen.add(rel)
 
-                if rel in seen:
-                    continue
-                seen.add(rel)
-
-                print(rel)
+                    print(rel)
 
 print("}")
