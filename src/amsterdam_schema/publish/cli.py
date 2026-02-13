9c1a6c30410a8f3e866c611bb1db5a1e9ae1a73e
lennard <lennard@stam.cc>
2026-02-13 15:14:29 +0100

    add parameters to  DefaultAzureCredential for blob upload to SA

--- src/amsterdam_schema/publish/cli.py
+++ src/amsterdam_schema/publish/cli.py
@@ -166,10 +166,10 @@ def azure_blob_uploader(
     html_content_settings = ContentSettings(content_type="text/html")
     sql_content_settings = ContentSettings(content_type="text/plain")
 
-    default_credential = ClientSecretCredential(
-    tenant_id=os.environ["AZURE_TENANT_ID"],
-    client_id=os.environ["AZURE_CLIENT_ID"],
-    client_secret=os.environ["AZURE_CLIENT_SECRET"])
+    tenant_id = os.environ["AZURE_TENANT_ID"]
+    client_id = os.environ["AZURE_CLIENT_ID"]
+    client_secret = os.environ["AZURE_CLIENT_SECRET"]
+    default_credential = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
     if default_credential is None:
         raise Exception("SCHEMAS_SA_KEY not set")
 
